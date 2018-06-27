b2b12d94ce385f07b3311924af486df7c4c39608
Kostyafffff <kostyan68@gmail.com>
2018-06-27 23:52:09 +0300

    Task №1

--- src/test/java/firstTest/Test1.java
+++ src/test/java/firstTest/Test1.java
@@ -1,11 +1,36 @@
 package firstTest;
 
+import static java.lang.Math.pow;
+
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+         /*
+    1) Если а – четное посчитать а*б/с,
+    если "а+с" или "b+c" четное - посчитать "a" возведенную в степень "c"
+    иначе посчитать  а+б-с
+     */
+         System.out.println(task1(0, -1, 1));
+
     }
 
-    public static int mult(int a, int b) {
-        return a * b;
+    /*
+    1) Если а – четное посчитать а*б/с,
+    если "а+с" или "b+c" четное - посчитать "a" возведенную в степень "c"
+    иначе посчитать  а+б-с
+     */
+    public static int task1(int a, int b, int c)
+    {
+        if(a % 2 == 0)
+        {
+            return a * b / c;
+        }
+        else if((a + c | b + c)% 2 == 0)
+        {
+            return (int) pow(a, c);
+        }
+        else
+        {
+            return a + b - c;
+        }
     }
 }
