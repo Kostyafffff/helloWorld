804b2f972307d77d5e27b352c3f2922a082a03a5
Kostyafffff <kostyan68@gmail.com>
2018-06-28 00:22:32 +0300

    Запаялся на факториале :)

--- src/test/java/firstTest/Test1.java
+++ src/test/java/firstTest/Test1.java
@@ -1,5 +1,8 @@
 package firstTest;
 
+import java.math.BigInteger;
+
+import static java.lang.Math.decrementExact;
 import static java.lang.Math.pow;
 
 public class Test1 {
@@ -9,7 +12,14 @@ public class Test1 {
     если "а+с" или "b+c" четное - посчитать "a" возведенную в степень "c"
     иначе посчитать  а+б-с
      */
+
+         System.out.println(doWhileFact(5));
          System.out.println(task1(0, -1, 1));
+
+         /*
+        2) Определить какой области принадлежит точка с координатами (х,у,z)
+          */
+
 
     }
 
@@ -18,19 +28,69 @@ public class Test1 {
     если "а+с" или "b+c" четное - посчитать "a" возведенную в степень "c"
     иначе посчитать  а+б-с
      */
-    public static int task1(int a, int b, int c)
-    {
-        if(a % 2 == 0)
-        {
+    public static int task1(int a, int b, int c) {
+        if (a % 2 == 0) {
             return a * b / c;
-        }
-        else if((a + c | b + c)% 2 == 0)
-        {
+        } else if ((a + c | b + c) % 2 == 0) {
             return (int) pow(a, c);
-        }
-        else
-        {
+        } else {
             return a + b - c;
         }
     }
+        public static int factForFirst( int number)
+        {
+            int mult = 1;
+            for(int i = 1; number >= 1; number--)
+            {
+                mult *= number;
+            }
+            return mult;
+        }
+
+        public static int factForSecond( int number)
+        {
+            int mult = 1;
+            for(int i = 1; i <= number; i++)
+            {
+                mult *= i;
+            }
+            return mult;
+        }
+
+        public static int whileFact(int number)
+        {
+            int i = 1;
+            int mult = 1;
+            while (i <= number)
+            {
+                mult *= i;
+                i++;
+            }
+            return mult;
+        }
+
+        public static int doWhileFact(int number)
+        {
+            int i = 1;
+            int mult = 1;
+            do{
+                mult *= i;
+                i++;
+            }while(i <= number);
+            return mult;
+        }
+
+        //если на выход получим огроменное число
+    public static BigInteger factorialBig(int number)
+    {
+        BigInteger big = BigInteger.ONE;
+        for(int i = 1; i <= number; i++)
+        {
+            big = big.multiply(BigInteger.valueOf(i));
+        }
+        return big;
+    }
+
+
 }
+
